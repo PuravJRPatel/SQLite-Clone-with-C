e5c70d369df9c25526a06959019ce334409be57b
Purav Patel <puravpatel@Puravs-MacBook-Air.local>
2025-04-15 19:16:14 +0530

    Achieved clarity on concept of Pointers and process of MALLOC

--- main.c
+++ main.c
@@ -3,37 +3,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct{
+typedef struct {
     char* buffer;
     size_t buffer_length;
-    __ssize_t input_length; 
+    ssize_t input_length;
 } InputBuffer;
 
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
-    input_buffer->buffer = NULL;
-    input_buffer->buffer_length = 0;
-    input_buffer->input_length = 0;
+    input_buffer -> buffer = NULL;
+    input_buffer -> buffer_length = 0;
+    input_buffer -> input_length = 0;
 
     return input_buffer;
 }
 
-void print_prompt() { printf ("db > "); }
-
+void print_prompt() {
+    printf("db > "); 
+}
 
 void read_input(InputBuffer* input_buffer) {
-    __ssize_t bytes_read = getline(&(input_buffer -> buffer), &(input_buffer -> buffer_length), stdin);
+    ssize_t bytes_read = getline(&(input_buffer -> buffer), &(input_buffer -> buffer_length), stdin);
+
     if (bytes_read <= 0) {
         printf("Error reading input\n");
         exit(EXIT_FAILURE);
     }
 
-    input_buffer->input_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read - 1] = 0; 
+    input_buffer -> input_length = bytes_read - 1;
+    input_buffer -> buffer[bytes_read - 1] = 0;
 }
 
 void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer);
+    free(input_buffer -> buffer);
     free(input_buffer);
 }
 
@@ -43,14 +45,11 @@ int main(int argc, char* argv[]) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        if (strcmp(input_buffer -> buffer, ".exit") == 0) {
             close_input_buffer(input_buffer);
             exit(EXIT_SUCCESS);
         } else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+            printf("Unrecognized command '%s'.\n", input_buffer -> buffer);
         }
     }
 }
-
-
-
